29b300cf1f139de1311500149a3a974e7226980f
Will Ro <wooro@umich.edu>
2017-12-21 03:39:22 -0500

    Add files via upload

--- teemo.py
+++ teemo.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
 import json
+from sklearn.externals import joblib
+import pickle
 
 class GetGameData(object):
     '''
@@ -89,8 +91,7 @@ class GetGameData(object):
         id_dict = self.generate_id(names) # dict
         placeholder = []
         for k, v in id_dict.items():
-            api = '/lol/match/v3/matchlists/by-account/{}'\
-                                        .format(v['accountId'])
+            api = '/lol/match/v3/matchlists/by-account/{}'.format(v['accountId'])
             content = json.loads(requests\
                 .get('https://na1.api.riotgames.com/{}?api_key={}'\
                 .format(api, self.key)).content)
@@ -99,22 +100,77 @@ class GetGameData(object):
         return dataframe
 
     def generate_player_history(self, names):
-    	games = self.generate_match_data(names)
-    	placeholder = []
-    	game_id = []
-    	for i in [x for x in games['matches'].values]:
-    		game_id.append(i['gameId'])
-    	for i in game_id:
-    		api = '/lol/match/v3/matches/{}'.format(i)
-    		content = json.loads(requests.get('https://na1.api.riotgames.com/{}?api_key={}'\
-    				.format(api, self.key)).content)
-    		placeholder.append(content)
-    	history_list = []
-    	for i in placeholder:
-    		for x in range(0, 9): # participant id's (1 - 10) needed to access player stats
-    			try:
-    				if i['participantIdentities'][x]['player']['summonerName'] in names:
-    					history_list.append(i['participants'][x]['stats'])
-    			except:
-    				break # iteration ends without KeyError
-    	return pd.DataFrame(history_list)
+        games = self.generate_match_data(names)
+        placeholder = []
+        game_id = []
+        for i in [x for x in games['matches'].values]:
+            game_id.append(i['gameId'])
+        for i in game_id:
+            api = '/lol/match/v3/matches/{}'.format(i)
+            content = json.loads(requests.get('https://na1.api.riotgames.com/{}?api_key={}'\
+                    .format(api, self.key)).content)
+            placeholder.append(content)
+        history_list = []
+        for i in placeholder:
+            for x in range(0, 9): # participant id's (1 - 10) needed to access player stats
+                try:
+                    if i['participantIdentities'][x]['player']['summonerName'] in names:
+                        history_list.append(i['participants'][x]['stats'])
+                except:
+                    break # iteration ends without KeyError
+        return pd.DataFrame(history_list)
+
+    def predict_my_wins(self, names):
+        print('Fetching data...')
+        hist = self.generate_player_history(names) # this is a dataframe of player history
+
+        col_list = ['win', 'item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'kills',
+        'deaths', 'assists', 'largestkillingspree', 'largestmultikill',
+        'killingsprees', 'longesttimespentliving', 'doublekills', 'triplekills',
+        'quadrakills', 'pentakills', 'magicdmgdealt', 'largestcrit',
+        'magicdmgtochamp', 'dmgselfmit', 'dmgtoobj', 'dmgtoturrets',
+        'visionscore', 'magicdmgtaken', 'goldearned', 'goldspent',
+        'turretkills', 'inhibkills', 'neutralminionskilled', 'ownjunglekills',
+        'enemyjunglekills', 'champlvl', 'wardsplaced', 'wardskilled',
+        'firstblood']
+
+        change_dict = {
+        'champlevel' : 'champlvl',
+        'damagedealttoturrets':'dmgtoturrets',
+        'damageselfmitigated' : 'dmgselfmit',
+        'firstbloodkill' : 'firstblood',
+        'inhibitorkills' : 'inhibkills',
+        'largestcriticalstrike' : 'largestcrit',
+        'magicdamagedealt' : 'magicdmgdealt',
+        'magicdamagedealttochampions' : 'magicdmgtochamp',
+        'magicaldamagetaken' : 'magicdmgtaken',
+        'neutralminionskilledenemyjungle' : 'enemyjunglekills',
+        'neutralminionskilledteamjungle' : 'ownjunglekills',
+        'damagedealttoobjectives' : 'dmgtoobj'}
+        hist.columns = hist.columns.map(lambda x: x.lower())
+        hist = hist.fillna(0)
+        hist = hist.rename(columns=change_dict)
+
+        print('Reshaping Data...')
+
+        for col in hist.columns:
+            if col not in col_list:
+                hist.drop(col, axis = 1, inplace = True)
+
+        # reorder one of the DataFrames to match the order of columns
+        feed = pd.DataFrame()
+        for col in hist.columns:
+            feed[col] = hist[col]
+
+        print('Making predictions....')
+
+        clf = pickle.load(open('model.sav', 'rb'))
+        
+        preds = clf.predict(feed.drop('win', axis=1))
+        pred_proba = clf.predict_proba(feed.drop('win', axis=1))
+        r2_score = clf.score(feed.drop('win', axis=1), feed.win)
+        print('predictions = ', preds)
+        print('predict_probas = ', pred_proba)
+        print('r2 score = ', r2_score)
+
+        return feed
